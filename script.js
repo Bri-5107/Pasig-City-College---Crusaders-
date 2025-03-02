c010247c7d955a74fe6577e85c1fe4f456d1a1d1
Brian Dionisio <briandionisio@Arlenes-MacBook-Air.local>
2025-03-03 00:55:54 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+/* Officers */
+
+function openLightbox(imgSrc, name, title) {
+    document.getElementById('lightbox-img').src = imgSrc;
+    document.getElementById('lightbox-name').innerText = name;
+    document.getElementById('lightbox-title').innerText = title;
+    document.getElementById('lightbox').style.display = "flex";
+}
+
+function closeLightbox() {
+    document.getElementById('lightbox').style.display = "none";
+}
+
+// Close lightbox when clicking outside the content
+document.getElementById('lightbox').addEventListener('click', function(event) {
+    if (event.target === this) {
+        closeLightbox();
+    }
+});
+
+/* Survey */
+
+
+
+
